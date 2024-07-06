54f2dc04eada3be0c3b02603bf6c6e6936f8f110
Alessandro Cotrufo <a.cotrufo@gmail.com>
2024-07-06 19:05:28 +0200

    Added tests for config

---
+++ tests/test_config.py
@@ -0,0 +1,42 @@
+import pytest
+import pdf2doi
+import os
+import pdf2doi.config as config
+
+default_config_params = {
+    "verbose": True,
+    "separator": os.path.sep,
+    "method_dxdoiorg": "application/citeproc+json",
+    "webvalidation": True,
+    "websearch": True,
+    "numb_results_google_search": 6,
+    "N_characters_in_pdf": 1000,
+    "save_identifier_metadata": True,
+    "replace_arxivID_by_DOI_when_available": True,
+}
+
+
+def test_config():
+    config.update_params(default_config_params)
+    for k in default_config_params:
+        config.set(k, default_config_params[k])
+
+
+def test_read_write_config():
+    new_params = {
+        "verbose": False,
+        "separator": os.path.sep,
+        "method_dxdoiorg": "application/citeproc+json",
+        "webvalidation": False,
+        "websearch": False,
+        "numb_results_google_search": 10,
+        "N_characters_in_pdf": 100,
+        "save_identifier_metadata": False,
+        "replace_arxivID_by_DOI_when_available": False,
+    }
+    config.update_params(new_params)
+    config.WriteParamsINIfile()
+    config.update_params(default_config_params)
+    config.ReadParamsINIfile()
+    for k in new_params:
+        config.set(k, new_params[k])
