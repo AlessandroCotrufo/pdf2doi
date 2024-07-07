5532c07dca6ffd3bea9140eca362089d67a7f587
Alessandro Cotrufo <a.cotrufo@gmail.com>
2024-07-07 22:27:43 +0200

    Changed configuration setup to allow for selecting finders methods

--- pdf2doi/config.py
+++ pdf2doi/config.py
@@ -1,8 +1,8 @@
-import configparser
+import toml
 import os
 import logging
 
-''' 
+""" 
 method_dxdoiorg                         It sets which method is used when querying dx.doi.org to retrieve the bibtex info
                                         Two possible values are 'text/bibliography; style=bibtex' , 'application/x-bibtex' and
                                         'application/citeproc+json'
@@ -33,22 +33,29 @@ save_identifier_metadata                Sets the default value of the global set
                                  
 'replace_arxivID_by_DOI_when_available' 
 
-'''
+"""
 
 
-class config():
-    __params={'verbose'   :   True,
-            'separator' : os.path.sep,
-            'method_dxdoiorg' : 'application/citeproc+json',
-            'webvalidation' : True,
-            'websearch' : True,
-            'numb_results_google_search' : 6,
-            'N_characters_in_pdf' : 1000,
-            'save_identifier_metadata' : True,
-            'replace_arxivID_by_DOI_when_available' : True
-            }
+class config:
+    __params = {
+        "verbose": True,
+        "separator": os.path.sep,
+        "method_dxdoiorg": "application/citeproc+json",
+        "webvalidation": True,
+        "websearch": True,
+        "numb_results_google_search": 6,
+        "N_characters_in_pdf": 1000,
+        "save_identifier_metadata": True,
+        "replace_arxivID_by_DOI_when_available": True,
+        "finders_methods": [
+            "document_infos",
+            "document_text",
+            "filename",
+            "title_google",
+            "first_N_characters_google",
+        ],
+    }
     __setters = __params.keys()
-
 
     @staticmethod
     def update_params(new_params):
@@ -61,77 +68,68 @@ class config():
     @staticmethod
     def set(name, value):
         if name in config.__setters:
-             config.__params[name] = value
+            config.__params[name] = value
         else:
             raise NameError("Name not accepted in set() method")
-        #Here we define additional actions to perform when specific parameters are modified
-        if name == 'verbose':
+        # Here we define additional actions to perform when specific parameters are modified
+        if name == "verbose":
             # We change the logger verbosity
-            if value: loglevel = logging.INFO
-            else: loglevel = logging.CRITICAL
+            if value:
+                loglevel = logging.INFO
+            else:
+                loglevel = logging.CRITICAL
             logger = logging.getLogger("pdf2doi")
             logger.setLevel(level=loglevel)
 
     @staticmethod
     def ReadParamsINIfile():
-        '''
-        Reads the parameters stored in the file settings.ini, and stores them in the dict self.params
+        """
+        Reads the parameters stored in the file settings.toml, and stores them in the dict self.params
         If the .ini file does not exist, it creates it with the default values.
-        '''
+        """
         path_current_directory = os.path.dirname(__file__)
-        path_config_file = os.path.join(path_current_directory, 'settings.ini')
-        if not(os.path.exists(path_config_file)):
+        path_config_file = os.path.join(path_current_directory, "settings.toml")
+        if not (os.path.exists(path_config_file)):
             config.WriteParamsINIfile()
         else:
-            config_object = configparser.ConfigParser()
-            config_object.optionxform = str
-            config_object.read(path_config_file)
-            config.__params.update(dict(config_object['DEFAULT']))
-            config.ConvertParamsToBool()
-            config.ConvertParamsToNumb()
+            with open(path_config_file, "r") as f:
+                config_dict = toml.load(f)
+            config.__params.update(config_dict)
+            # config.ConvertParamsToBool()
+            # config.ConvertParamsToNumb()
 
     @staticmethod
     def ConvertParamsToBool():
-        for key,val in config.__params.items():
+        for key, val in config.__params.items():
             if isinstance(val, str):
-                if val.lower() == 'true':
-                    config.__params[key]=True
-                if val.lower() == 'false':
-                    config.__params[key]=False
+                if val.lower() == "true":
+                    config.__params[key] = True
+                if val.lower() == "false":
+                    config.__params[key] = False
 
     @staticmethod
     def ConvertParamsToNumb():
-        for key,val in config.__params.items():
+        for key, val in config.__params.items():
             if isinstance(val, str) and val.isdigit():
-                config.__params[key]=int(val)
+                config.__params[key] = int(val)
+
     @staticmethod
     def print():
-        '''
+        """
         Prints all settings
-        '''
-        for key,val in config.__params.items():
-            print(key + " : " + str(val) + ' ('+type(val).__name__+')')
+        """
+        for key, val in config.__params.items():
+            print(key + " : " + str(val) + " (" + type(val).__name__ + ")")
 
     @staticmethod
     def WriteParamsINIfile():
-        '''
-        Writes the parameters currently stored in in the dict self.params into the file settings.ini
-        '''
+        """
+        Writes the parameters currently stored in in the dict self.params into the file settings.toml
+        """
         path_current_directory = os.path.dirname(__file__)
-        path_config_file = os.path.join(path_current_directory, 'settings.ini')
-        config_object = configparser.ConfigParser()
-        config_object.optionxform = str
-        config_object['DEFAULT'] = config.__params
-        with open(path_config_file, 'w') as configfile: #Write them on file
-            config_object.write(configfile)
+        path_config_file = os.path.join(path_current_directory, "settings.toml")
+        with open(path_config_file, "w") as f:  # Write them on file
+            toml.dump(config.__params, f)
 
 
 ###########################################
-
-
-
-
-
-
-
-
