6417131598af2d12c6c097463605606512259422
Alessandro Cotrufo <a.cotrufo@gmail.com>
2024-07-08 23:17:34 +0200

    Added new tests, removed commented code

--- tests/test_finders.py
+++ tests/test_finders.py
@@ -21,91 +21,216 @@ default_config_params = {
 
 config.update_params(default_config_params)
 
-valid_test_doi = r"10.1103/PhysRev.47.777".lower()
+valid_doi = r"10.1103/PhysRev.47.777".lower()
 valid_test_title = (
     r"Can Quantum Mechanical Description of Physical Reality Be Considered Complete"
 )
-wrong_test_doi = "foo"
-valid_arxiv_id = r"arXiv:acc-phys/9601001"
+wrong_doi = "foo"
+valid_arxiv_id = r"2407.03393"  # picked at random
 
 
 @pytest.fixture
-def valid_pdf_path(tmp_path):
+def path_to_pdf_with_valid_doi(tmp_path):
     file_path = tmp_path / (
-        valid_test_doi.replace("/", r"%2F") + " " + valid_test_title + ".pdf"
+        valid_doi.replace("/", r"%2F") + " " + valid_test_title + ".pdf"
     )
     doc = fitz.open()
-    doc.insert_page(-1, text=(valid_test_doi + " " + valid_test_title))
+    doc.insert_page(-1, text=(valid_doi + " " + valid_test_title))
     doc.save(file_path)
     doc.close()
     return file_path
 
 
 @pytest.fixture
-def wrong_pdf_path(tmp_path):
-    file_path = tmp_path / (wrong_test_doi + ".pdf")
+def path_to_pdf_with_valid_arxiv_id(tmp_path):
+    file_path = tmp_path / (valid_arxiv_id.replace("/", r"%2F") + ".pdf")
     doc = fitz.open()
-    doc.insert_page(-1, text=(wrong_test_doi))
+    doc.insert_page(-1, text=(valid_arxiv_id))
     doc.save(file_path)
     doc.close()
     return file_path
 
 
-def test_validate_doi_web():
-    r = pdf2doi.finders.validate_doi_web(valid_test_doi)
+@pytest.fixture
+def path_to_pdf_with_wrong_doi(tmp_path):
+    file_path = tmp_path / (wrong_doi + ".pdf")
+    doc = fitz.open()
+    doc.insert_page(-1, text=(wrong_doi))
+    doc.save(file_path)
+    doc.close()
+    return file_path
+
+
+def test_validate_doi_web_with_valid_doi():
+    r = pdf2doi.finders.validate_doi_web(valid_doi)
     assert isinstance(r, str)
 
-    r = pdf2doi.finders.validate_doi_web(wrong_test_doi)
+
+def test_validate_doi_web_with_wrong_doi():
+    r = pdf2doi.finders.validate_doi_web(wrong_doi)
     assert r is None
 
 
-def test_read_pdf_info(valid_pdf_path):
+def test_validate_arxiv_id_web_with_valid_arxiv_id():
+    r = pdf2doi.finders.validate_arxivID_web(valid_arxiv_id)
+    assert r is not None and r != -1
 
-    assert valid_pdf_path.exists()
 
-    pdf2doi.finders.add_found_identifier_to_metadata(
-        target=str(valid_pdf_path), identifier="test"
+def test_validate():
+    r = pdf2doi.finders.validate(valid_doi, what="doi")
+    assert isinstance(r, str)
+
+
+def test_extract_arxivID_from_text():
+    r = pdf2doi.finders.extract_arxivID_from_text(valid_arxiv_id, version=2)
+    assert isinstance(r, list)
+    assert len(r) == 1
+
+
+def test_extract_doi_from_text():
+    assert pdf2doi.extract_doi_from_text(valid_doi, version=1)[0] == valid_doi
+
+
+def test_find_identifier_in_google_search():
+
+    query = valid_doi + " " + valid_test_title + ".pdf"
+
+    identifier, desc, info = pdf2doi.find_identifier_in_google_search(
+        query=query, func_validate=pdf2doi.finders.validate, numb_results=6
     )
 
-    with open(valid_pdf_path, "rb") as f:
+    assert identifier == valid_doi
+
+
+def test_find_identifier_in_google_search_with_mock(mocker):
+
+    mock_search = mocker.patch("pdf2doi.finders.search")
+    mock_search.return_value = [
+        "https://journals.aps.org/pr/abstract/10.1103/PhysRev.47.777"
+    ]
+
+    mock_validate = mocker.patch("pdf2doi.finders.validate_doi_web")
+    mock_validate.return_value = valid_doi
+
+    # mock_get = mocker.patch('requests.get')
+    # mock_response = mocker.Mock()
+    # mock_response.text = valid_doi
+    # mock_get.return_value = mock_response
+
+    query = valid_doi + " " + valid_test_title + ".pdf"
+
+    identifier, desc, info = pdf2doi.find_identifier_in_google_search(
+        query=query, func_validate=pdf2doi.finders.validate, numb_results=6
+    )
+
+    mock_search.assert_called()
+    # mock_get.assert_called()
+    mock_search.assert_called_once_with(query, stop=6)
+    # mock_get.assert_called_once_with(mock_search.return_value[0])
+
+    assert identifier == valid_doi
+
+
+def test_find_identifier_in_text():
+    assert 0
+
+
+def test_add_and_get_pdf_info(path_to_pdf_with_valid_doi):
+
+    identifier = "test"
+
+    assert path_to_pdf_with_valid_doi.exists()
+
+    pdf2doi.finders.add_found_identifier_to_metadata(
+        target=str(path_to_pdf_with_valid_doi), identifier=identifier
+    )
+
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
         info = pdf2doi.finders.get_pdf_info(f)
 
     assert info is not None
+    assert "/pdf2doi_identifier" in info
+    assert info["/pdf2doi_identifier"] == identifier
 
 
-def test_find_identifier_in_pdf_info(valid_pdf_path):
+def test_get_pdf_info():
+    assert 0
 
-    assert valid_pdf_path.exists()
+
+def test_find_possible_titles():
+    assert 0
+
+
+def test_get_pdf_text():
+    assert 0
+
+
+def test_add_metadata():
+    assert 0
+
+
+def test_add_found_identifier_to_metadata():
+    assert 0
+
+
+# 0)
+def test_find_identifier():
+    assert 0
+
+
+# 1)
+def test_find_identifier_in_pdf_info_with_valid_doi(path_to_pdf_with_valid_doi):
+
+    assert path_to_pdf_with_valid_doi.exists()
 
     pdf2doi.finders.add_found_identifier_to_metadata(
-        target=str(valid_pdf_path), identifier=valid_test_doi
+        target=str(path_to_pdf_with_valid_doi), identifier=valid_doi
     )
 
-    with open(valid_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_in_pdf_info(
             f, func_validate=pdf2doi.finders.validate
         )
 
-    assert identifier == valid_test_doi
+    assert identifier == valid_doi
 
 
-def test_find_identifier_in_pdf_text(valid_pdf_path):
+def test_find_identifier_in_pdf_info_with_valid_arxiv_id(
+    path_to_pdf_with_valid_arxiv_id,
+):
 
-    assert valid_pdf_path.exists()
+    assert path_to_pdf_with_valid_arxiv_id.exists()
 
-    with open(valid_pdf_path, "rb") as f:
+    pdf2doi.finders.add_found_identifier_to_metadata(
+        target=str(path_to_pdf_with_valid_arxiv_id), identifier=valid_arxiv_id
+    )
+
+    with open(path_to_pdf_with_valid_arxiv_id, "rb") as f:
+        identifier, desc, info = pdf2doi.find_identifier_in_pdf_info(
+            f, func_validate=pdf2doi.finders.validate
+        )
+
+    assert identifier == valid_arxiv_id
+
+
+# 2)
+def test_find_identifier_in_pdf_text(path_to_pdf_with_valid_doi):
+
+    assert path_to_pdf_with_valid_doi.exists()
+
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_in_pdf_text(
             f, func_validate=pdf2doi.finders.validate
         )
 
-    assert identifier == valid_test_doi
+    assert identifier == valid_doi
 
 
-def test_find_identifier_in_pdf_text_fail(wrong_pdf_path):
+def test_find_identifier_in_pdf_text_fail(path_to_pdf_with_wrong_doi):
 
-    assert wrong_pdf_path.exists()
+    assert path_to_pdf_with_wrong_doi.exists()
 
-    with open(wrong_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_wrong_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_in_pdf_text(
             f, func_validate=pdf2doi.finders.validate
         )
@@ -113,23 +238,24 @@ def test_find_identifier_in_pdf_text_fail(wrong_pdf_path):
     assert identifier is None
 
 
-def test_find_identifier_in_filename(valid_pdf_path):
+# 3)
+def test_find_identifier_in_filename(path_to_pdf_with_valid_doi):
 
-    assert valid_pdf_path.exists()
+    assert path_to_pdf_with_valid_doi.exists()
 
-    with open(valid_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_in_filename(
             f, func_validate=pdf2doi.finders.validate
         )
 
-    assert identifier == valid_test_doi
+    assert identifier == valid_doi
 
 
-def test_find_identifier_in_filename_fail(wrong_pdf_path):
+def test_find_identifier_in_filename_fail(path_to_pdf_with_wrong_doi):
 
-    assert wrong_pdf_path.exists()
+    assert path_to_pdf_with_wrong_doi.exists()
 
-    with open(wrong_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_wrong_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_in_filename(
             f, func_validate=pdf2doi.finders.validate
         )
@@ -137,23 +263,24 @@ def test_find_identifier_in_filename_fail(wrong_pdf_path):
     assert identifier is None
 
 
-def test_find_identifier_by_googling_title(valid_pdf_path):
+# 4)
+def test_find_identifier_by_googling_title(path_to_pdf_with_valid_doi):
 
-    assert valid_pdf_path.exists()
+    assert path_to_pdf_with_valid_doi.exists()
 
-    with open(valid_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_by_googling_title(
             f, func_validate=pdf2doi.finders.validate
         )
 
-    assert identifier == valid_test_doi
+    assert identifier == valid_doi
 
 
-def test_find_identifier_by_googling_title_fail(wrong_pdf_path):
+def test_find_identifier_by_googling_title_fail(path_to_pdf_with_wrong_doi):
 
-    assert wrong_pdf_path.exists()
+    assert path_to_pdf_with_wrong_doi.exists()
 
-    with open(wrong_pdf_path, "rb") as f:
+    with open(path_to_pdf_with_wrong_doi, "rb") as f:
         identifier, desc, info = pdf2doi.find_identifier_by_googling_title(
             f, func_validate=pdf2doi.finders.validate
         )
@@ -161,33 +288,58 @@ def test_find_identifier_by_googling_title_fail(wrong_pdf_path):
     assert identifier is None
 
 
-def test_find_identifier_by_googling_first_N_characters_in_pdf(valid_pdf_path):
+# 5)
+def test_find_identifier_by_googling_first_N_characters_in_pdf(
+    path_to_pdf_with_valid_doi,
+):
 
-    assert valid_pdf_path.exists()
+    assert path_to_pdf_with_valid_doi.exists()
 
-    with open(valid_pdf_path, "rb") as f:
-        identifier, desc, info = (
-            pdf2doi.find_identifier_by_googling_first_N_characters_in_pdf(
-                f, func_validate=pdf2doi.finders.validate
-            )
+    with open(path_to_pdf_with_valid_doi, "rb") as f:
+        (
+            identifier,
+            desc,
+            info,
+        ) = pdf2doi.find_identifier_by_googling_first_N_characters_in_pdf(
+            f, func_validate=pdf2doi.finders.validate
         )
 
-    assert identifier == valid_test_doi
+    assert identifier == valid_doi
 
 
-def test_find_identifier_by_googling_first_N_characters_in_pdf_fail(wrong_pdf_path):
+def test_find_identifier_by_googling_first_N_characters_in_pdf_fail(
+    path_to_pdf_with_wrong_doi,
+):
 
-    assert wrong_pdf_path.exists()
+    assert path_to_pdf_with_wrong_doi.exists()
 
-    with open(wrong_pdf_path, "rb") as f:
-        identifier, desc, info = (
-            pdf2doi.find_identifier_by_googling_first_N_characters_in_pdf(
-                f, func_validate=pdf2doi.finders.validate
-            )
+    with open(path_to_pdf_with_wrong_doi, "rb") as f:
+        (
+            identifier,
+            desc,
+            info,
+        ) = pdf2doi.find_identifier_by_googling_first_N_characters_in_pdf(
+            f, func_validate=pdf2doi.finders.validate
         )
 
     assert identifier is None
+
+
+# extra
+def test_file_size_change_after_changing_metadata(path_to_pdf_with_valid_doi: Path):
+
+    assert path_to_pdf_with_valid_doi.exists()
+
+    file_size_before = path_to_pdf_with_valid_doi.stat().st_size
+
+    pdf2doi.finders.add_found_identifier_to_metadata(
+        target=str(path_to_pdf_with_valid_doi), identifier=valid_doi
+    )
+
+    file_size_after = path_to_pdf_with_valid_doi.stat().st_size
+
+    assert (file_size_after - file_size_before) < 1000
 
 
 if __name__ == "__main__":
-    pass
+    pytest.main()
--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -1,9 +1,6 @@
 import pytest
 import re
-from pdf2doi.patterns import (
-    doi_regexp,
-    standardise_doi
-)
+from pdf2doi.patterns import doi_regexp, standardise_doi
 
 BASIC_DOIS = [
     "10.1006/jmrb.1993.1004",
@@ -15,22 +12,20 @@ BASIC_DOIS = [
     "10.1109/sibgrapi.2012.5",
     "10.1111/j.1532-5415.2012.04014.x",
     "10.2307/3950104",
-    "10.1002/esp.3322"
+    "10.1002/esp.3322",
 ]
 
-DOIS_WITH_NON_STANDARD_SEPARTORS = [
-    doi.replace("/", ":") for doi in BASIC_DOIS
-] + [
-    doi.replace("/", " ") for doi in BASIC_DOIS
-] + [
-    f"[{doi.replace('/', ']')}" for doi in BASIC_DOIS
-]
+DOIS_WITH_NON_STANDARD_SEPARTORS = (
+    [doi.replace("/", ":") for doi in BASIC_DOIS]
+    + [doi.replace("/", " ") for doi in BASIC_DOIS]
+    + [f"[{doi.replace('/', ']')}" for doi in BASIC_DOIS]
+)
 
 # Also includes those with dots
 DOIS_WITH_SHORT_NAMESPACES = [
     "10.2.337/dc08-2337",
     "10.58.12/numonthly.14189",
-    "10.16/j.reuma.2008.12.011"
+    "10.16/j.reuma.2008.12.011",
 ]
 
 # Note that these are NOT supported, but useful to collate in future versions
@@ -38,29 +33,40 @@ STRANGE_BUT_VALID_DOIS = [
     "10.1642/0004-8038(2005)122[0121:POTPIS]2.0.CO;2",
     "10.1002/1521-4141(200106)31:6<1685::aid-immu1685>3.0.co;2-v",
     "10.1676/0043-5643(2002)114[0197:rbacib]2.0.co;2",
-    "10.1061/(asce)0733-9429(2008)134:4(390)" # terminates in a closing parenthesis
+    "10.1061/(asce)0733-9429(2008)134:4(390)",  # terminates in a closing parenthesis
 ]
 
-@pytest.mark.parametrize(["suspected", "expected"], [
-    ["10.1177:0146167297234003", "10.1177/0146167297234003"],
-    ["10.1109/CVPR.2016.90.", "10.1109/cvpr.2016.90"],
-    *zip(DOIS_WITH_NON_STANDARD_SEPARTORS, BASIC_DOIS + BASIC_DOIS + BASIC_DOIS)
-])
-def test_standardise_doi(suspected, expected): 
+
+@pytest.mark.parametrize(
+    ["suspected", "expected"],
+    [
+        ["10.1177:0146167297234003", "10.1177/0146167297234003"],
+        ["10.1109/CVPR.2016.90.", "10.1109/cvpr.2016.90"],
+        *zip(DOIS_WITH_NON_STANDARD_SEPARTORS, BASIC_DOIS + BASIC_DOIS + BASIC_DOIS),
+    ],
+)
+def test_standardise_doi(suspected, expected):
     assert standardise_doi(suspected) == expected
 
-@pytest.mark.parametrize(["suspected", "expected"], [
-    *zip(BASIC_DOIS, BASIC_DOIS),
-    ["10.1109/sp.2011.40"] * 2,
-    ["doi10.1177:0146167297234003", "10.1177/0146167297234003"],
-    ["10.1177:0146167297234003.pdf", "10.1177/0146167297234003.pdf"],
-    ["https://journals.sagepub.com/doi/pdf/10.1177/0146167297234003", "10.1177/0146167297234003"],
-    ["https://doi.org/10.1109/sp.2011.40", "10.1109/sp.2011.40"]
-])
+
+@pytest.mark.parametrize(
+    ["suspected", "expected"],
+    [
+        *zip(BASIC_DOIS, BASIC_DOIS),
+        ["10.1109/sp.2011.40"] * 2,
+        ["doi10.1177:0146167297234003", "10.1177/0146167297234003"],
+        ["10.1177:0146167297234003.pdf", "10.1177/0146167297234003.pdf"],
+        [
+            "https://journals.sagepub.com/doi/pdf/10.1177/0146167297234003",
+            "10.1177/0146167297234003",
+        ],
+        ["https://doi.org/10.1109/sp.2011.40", "10.1109/sp.2011.40"],
+    ],
+)
 def test_is_loose_doi_match(suspected, expected):
     print(suspected)
     for ver, regex in enumerate(doi_regexp):
-        
+
         identifiers = re.findall(regex, suspected.lower())
         if identifiers:
             print(f"Matched with {ver} - {identifiers}")
