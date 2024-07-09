6143737d31c659972843b665e35411de12c2e644
Alessandro Cotrufo <a.cotrufo@gmail.com>
2024-07-09 22:42:37 +0200

    Turned off test which changed actual settings file

--- tests/test_config.py
+++ tests/test_config.py
@@ -23,22 +23,24 @@ def test_config():
     for k in default_config_params:
         config.set(k, default_config_params[k])
 
-
-def test_read_write_config():
-    new_params = {
-        "verbose": False,
-        "separator": os.path.sep,
-        "method_dxdoiorg": "application/citeproc+json",
-        "webvalidation": False,
-        "websearch": False,
-        "numb_results_google_search": 10,
-        "N_characters_in_pdf": 100,
-        "save_identifier_metadata": False,
-        "replace_arxivID_by_DOI_when_available": False,
-    }
-    config.update_params(new_params)
-    config.WriteParamsINIfile()
-    config.update_params(default_config_params)
-    config.ReadParamsINIfile()
-    for k in new_params:
-        config.set(k, new_params[k])
+# this test actually changes the settings file used by the tool
+# this change should be either mocker, or restored, or applied to a temporary file
+# it would require the read and write functions to be edited so they accept a path for settings file
+# def test_read_write_config():
+#     new_params = {
+#         "verbose": False,
+#         "separator": os.path.sep,
+#         "method_dxdoiorg": "application/citeproc+json",
+#         "webvalidation": False,
+#         "websearch": False,
+#         "numb_results_google_search": 10,
+#         "N_characters_in_pdf": 100,
+#         "save_identifier_metadata": False,
+#         "replace_arxivID_by_DOI_when_available": False,
+#     }
+#     config.update_params(new_params)
+#     config.WriteParamsINIfile()
+#     config.update_params(default_config_params)
+#     config.ReadParamsINIfile()
+#     for k in new_params:
+#         config.set(k, new_params[k])
